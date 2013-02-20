f2d0ea1f7752092967522c61f96bd4eed191c49a
Jan Vennemann <rafik@gmx.net>
2013-02-20 20:41:13 +0100

    LayerSlider integriert
    Funktioniert noch nicht ganz wie es sollte, aber zwei Layer hab ich
    schon mal drin!

--- html_template/js/init.js
+++ html_template/js/init.js
@@ -1,13 +1,25 @@
 $(document).ready(function() {
 	$("#logo").fadeIn(750);
 
-});
+	// Responsive Keyboard Shortcuts DEMO
+	Mousetrap.bind('h', function() {
+		$("#logo").fadeOut(750);
+	 });
 
-// Responsive Keyboard Shortcuts DEMO
-Mousetrap.bind('h', function() { 
-	$("#logo").fadeOut(750);
- });
- 
-Mousetrap.bind('s', function() { 
-	$("#logo").fadeIn(750);
- });
+	Mousetrap.bind('s', function() {
+		$("#logo").fadeIn(750);
+	 });
+
+	/**
+	 * LayerSlider
+	 */
+	$('#layerslider').layerSlider({
+	    skinsPath : 'layerslider/skins/',
+	    skin : 'fullwidth',
+	    thumbnailNavigation : 'hover',
+	    hoverPrevNext : false,
+	    responsive : false,
+	    responsiveUnder : 940,
+	    sublayerContainer : 900
+	});
+});
